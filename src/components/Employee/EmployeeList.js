e85b2564d1497d4a8683fa0dfd797bb513add4a1
monano <mona.gaihab@gmail.com>
2020-02-02 15:16:22 +0300

    added Employee Form file

---
+++ src/components/Employee/EmployeeList.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import ListGroup from "bootstrap"
+import EmployeeItem from "./EmployeeItem"
+import axios from 'axios'
+import apiUrl from '../../apiConfig'
+
+class EmployeeList extends Component {
+    state={
+        employees:[]
+    }
+    componentDidMount(){
+        const user = this.props.user
+        axios({
+            method:'GET',
+            url:apiUrl+'/departments',
+            headers:{
+                "Authorization":`Bearer ${user.token}`
+            }
+        })
+        .then((res)=>{
+            this.setState(({...copyState})=>{
+                copyState = res.data
+                return copyState
+            })
+        })
+        .catch(error=>alert.error(error))
+    }
+    render() { 
+        return ( 
+            <ListGroup>
+                {this.state.employees.map((employee,index) => ( 
+                    <EmployeeItem key={index} employee_name={employee.name}></EmployeeItem>
+                ))}     
+            </ListGroup>
+        );
+    }
+}
+ 
+export default EmployeeList;
