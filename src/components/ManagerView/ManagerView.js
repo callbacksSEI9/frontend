36c22c0a6b13af748369f771f9ac7ded9e6f012c
ahmedalnemer <ahmedalnemer3@gmail.com>
2020-02-02 11:43:22 +0300

    added components directory

---
+++ src/components/ManagerView/ManagerView.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ManagerView extends Component {
+    state = {  }
+    render() { 
+        return (
+            <div>
+                
+            </div>
+          );
+    }
+}
+ 
+export default ManagerView;
