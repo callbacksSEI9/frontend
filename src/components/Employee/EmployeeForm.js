15c48f1199d30647f837e126a101252f862af4bc
monano <mona.gaihab@gmail.com>
2020-02-05 12:31:43 +0300

    Edited and Cleaned the Code In the EmployeeForm File

--- src/components/Employee/EmployeeForm.js
+++ src/components/Employee/EmployeeForm.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react'
-import { withRouter, Route } from 'react-router-dom'
-
-import { signUp } from '../../auth/api'
-import messages from '../../auth/messages'
-import Dropdown from 'react-bootstrap/Dropdown'
+import { withRouter } from 'react-router-dom'
 import DropdownButton from 'react-bootstrap/DropdownButton'
 import DropdownItem from 'react-bootstrap/DropdownItem'
 import axios from 'axios'
@@ -27,36 +23,14 @@ class EmployeeForm extends Component {
     })
     .catch(error=>alert(error))
     
-}
-//************************ This Was When Manager Sign Up The Employee **********************
-  // handleChange = event => this.setState({
-  //   [event.target.name]: event.target.value
-  // })
-
-  // onSignUp = event => {
-  //   event.preventDefault()
-
-  //   const { alert } = this.props
-
-  //   signUp(this.state)
-  //   //   .then(() => signIn(this.state))
-  //   //   .then(res => setUser(res.data.user))
-  //     .then(() => alert(messages.addEmployeeSuccess, 'success'))
-  //   //   .then(() => history.push('/'))
-  //     .catch(error => {
-  //       console.error(error)
-  //       this.setState({ email: '', password: '', passwordConfirmation: '' })
-  //       alert(messages.addEmployeeFailure, 'danger')
-  //     })
-  // }
-//************************ END **********************
+  }
 
   handleSelect = (empEmail) =>{
     let user = this.props.user
     let credentials={
       email:empEmail
     }
-     axios({
+      axios({
       url: apiUrl + '/departments',
       method: 'POST',
       data: {
@@ -68,66 +42,19 @@ class EmployeeForm extends Component {
       }
       }
     })
-    .then(
-      res => console.log(res.data)
-  )
-    // .then((res)=>{
-    //   //  let employees = res.data.users
-    //     // this.setState({employees:employees})
-    //     // this.setState(({...copyState})=>{
-    //     //     copyState = res.data.departments[0].employees
-    //     //     return copyState
-    //     // })
-    //     console.log(res)
-    // })
+    .then(res => console.log(res.data))
     .catch(error=>alert(error))
   }
-  render () {
-    // const { email, password, passwordConfirmation } = this.state
 
+  render () {
     return (
       <React.Fragment>
-       {/* <form className='auth-form' onSubmit={this.onSignUp}> */}
         <h3>Add New Employee</h3>
-        {/* <DropdownButton title='Dropdowna' onSelect={function(evt){console.log(evt)}}>
-  <DropdownItem eventKey='abc'>Dropdown link</DropdownItem>
-  <DropdownItem eventKey={['a', 'b']}>Dropdown link</DropdownItem>
-</DropdownButton> */}
-<DropdownButton title='Employees' onSelect={()=>{this.handleSelect()}}>
-
-    {this.state.employees.map((employee,index)=> 
-      <DropdownItem key={index} eventKey={employee.email}> {employee.name} </DropdownItem>
-    )}
-</DropdownButton>
-        {/* <label htmlFor="email">Email</label>
-        <input
-          required
-          name="email"
-          value={email}
-          type="email"
-          placeholder="Email"
-          onChange={this.handleChange}
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          required
-          name="password"
-          value={password}
-          type="password"
-          placeholder="Password"
-          onChange={this.handleChange}
-        />
-        <label htmlFor="passwordConfirmation">Confirm Password</label>
-        <input
-          required
-          name="passwordConfirmation"
-          value={passwordConfirmation}
-          type="password"
-          placeholder="Confirm Password"
-          onChange={this.handleChange}
-        /> */}
-        {/* <button type="submit">Add</button>
-      </form> */}
+        <DropdownButton title='Employees' onSelect={()=>{this.handleSelect()}}>
+          {this.state.employees.map((employee,index)=> 
+            <DropdownItem key={index} eventKey={employee.email}> {employee.name} </DropdownItem>
+          )}
+        </DropdownButton>
       </React.Fragment>
     )
   }
